5b4221207f12aec44610ab4cde2058f5969f013d
Irtiza Ashraf <irtizaashraf802@gmail.com>
2024-01-08 03:12:15 +0500

    done

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -8,13 +8,13 @@ function App() {
                         <div >
                               <h1 className='text-red-600 mt-3  font-Lobster  text-4xl'>Password Generator</h1>
                         </div>
-                        <div className='flex gap-12  text-black'>
-                              <p className='font-poppins mt-6'>Personal</p>
-                              <p className='font-poppins mt-6'>Bussiness</p>
-                              <p className='font-poppins mt-6'>Partners</p>
-                              <p className='font-poppins mt-6'>Contact</p>
-                              <p className='font-poppins mt-6'>Review</p>
-                              <button className='text-white mt-[14px] font-poppins px-6 py-2 bg-red-500 rounded-full'>Log In</button>
+                        <div className='flex gap-12  text-black '>
+                              <p className='font-poppins mt-6 hover:text-red-700'>Personal</p>
+                              <p className='font-poppins mt-6 hover:text-red-700'>Bussiness</p>
+                              <p className='font-poppins mt-6 hover:text-red-700'>Partners</p>
+                              <p className='font-poppins mt-6 hover:text-red-700'>Contact</p>
+                              <p className='font-poppins mt-6 hover:text-red-700'>Review</p>
+                              <button className='text-white mt-[14px] font-poppins px-6 py-2 bg-red-500 rounded-full hover:text-red-300 '>Log In</button>
                         </div>
                   </div>
             </>
